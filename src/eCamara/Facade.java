bf46ef305a0ee16b8d97ec13c4b41719138325a3
rodrigoec <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2019-06-11 16:15:44 -0300

    use cases 6 de daniel enviados(ta dando problema)

--- src/eCamara/Facade.java
+++ src/eCamara/Facade.java
@@ -11,10 +11,12 @@ public class Facade {
     }
 
     public static void main(String[] args) {
-        args = new String[] {"eCamara.Facade", "Projeto-LP2/acceptance_test/use_case_1.txt",
-                                               "Projeto-LP2/acceptance_test/use_case_2.txt",
-                                               "Projeto-LP2/acceptance_test/use_case_3.txt",
-                                               "Projeto-LP2/acceptance_test/use_case_4.txt", "Projeto-LP2/acceptance_test/use_case_5.txt"};
+        args = new String[] {"eCamara.Facade", "C:/Users/rodri/Documents/lp2/Projeto-LP2/acceptance_test/use_case_1.txt",
+                                               "C:/Users/rodri/Documents/lp2/Projeto-LP2/acceptance_test/use_case_2.txt",
+                                               "C:/Users/rodri/Documents/lp2/Projeto-LP2/acceptance_test/use_case_3.txt",
+                                               "C:/Users/rodri/Documents/lp2/Projeto-LP2/acceptance_test/use_case_4.txt",
+                                               "C:/Users/rodri/Documents/lp2/Projeto-LP2/acceptance_test/use_case_5.txt",
+                                               "C:/Users/rodri/Documents/lp2/Projeto-LP2/acceptance_test/use_case_6.txt"};
         EasyAccept.main(args);
     }
 
