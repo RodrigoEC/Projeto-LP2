1ec65d4d6689652ac6c70323ce94bfd07ffd6b69
claudiodantas <claudio_dantas@hotmail.com>
2019-06-21 09:37:41 -0300

    Implementção da estratégia CONSTITUCIONAL (Use Case 9)

--- src/eCamara/individuo/Aprovacao.java
+++ src/eCamara/individuo/Aprovacao.java
@@ -3,11 +3,12 @@ package eCamara.individuo;
 import eCamara.legislativo.ProjetoDeLei;
 
 import java.util.Map;
+import java.util.Set;
 
 public class Aprovacao implements EstrategiaProposta {
 
     @Override
-    public String pegarPropostaRelacionada(Map<String, ProjetoDeLei> leis, String interesses) {
+    public String pegarPropostaRelacionada(Map<String, ProjetoDeLei> leis, Set<String> interesses) {
         return null;
     }
 }
--- src/eCamara/individuo/Conclusao.java
+++ src/eCamara/individuo/Conclusao.java
@@ -3,11 +3,12 @@ package eCamara.individuo;
 import eCamara.legislativo.ProjetoDeLei;
 
 import java.util.Map;
+import java.util.Set;
 
 public class Conclusao implements EstrategiaProposta {
 
     @Override
-    public String pegarPropostaRelacionada(Map<String, ProjetoDeLei> leis, String interesses) {
+    public String pegarPropostaRelacionada(Map<String, ProjetoDeLei> leis, Set<String> interesses) {
         return null;
     }
 }
